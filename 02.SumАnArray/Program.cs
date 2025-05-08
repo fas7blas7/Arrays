2a79ad75416e1095ee7a7866fc0230867193117e
Martin Kirov <fas7blas7@gmail.com>
2025-05-08 21:26:19 +0300

    Adding SumAnArray task

---
+++ 02.SumАnArray/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int[] numbersArray = Console.ReadLine()        // четем стринг от конзолата
+                            .Split(" ")        // разделяме го на масив от стгрингове
+                            .Select(int.Parse) // всеки елемент го партсваме към int
+                            .ToArray();        // обръщаме финалната колекция в масив
+
+int sum = 0;
+
+for  (int i = 0; i < numbersArray.Length; i++)
+{
+    int currentNumber = numbersArray[i];
+
+    sum += currentNumber;
+}
+
+Console.WriteLine(sum);
